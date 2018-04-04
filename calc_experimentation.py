a4a487f9d836cd4a6c5ae7d7a28d236789429fd0
Max Friedman <mr.awesome10000@gmail.com>
2018-04-04 15:05:48 -0700

    stuff

--- calc_experimentation.py
+++ calc_experimentation.py
@@ -1,1 +1,9 @@
 # calc_experimentation.py
+
+
+	# intentianly not using compile_ignore_case so that you can
+	# differentiate between capital and lowercase units
+	conv_comp = re.compile(
+		"[Cc][Oo][Nn][Vv][Ee][Rr][Tt] (.+?)(?="
+		+ "|".join(units) + ")(?:" + "|".join(units) + ")[Tt][Oo] ("
+		+ "|".join(units) + ")"),
