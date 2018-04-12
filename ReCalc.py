7879a4e43b6538926f00e82d4af5a8e2d550ece0
Max Friedman <mr.awesome10000@gmail.com>
2018-04-12 13:27:49 -0700

    unit conversion and get version with argument to main program

--- ReCalc.py
+++ ReCalc.py
@@ -15,7 +15,7 @@ Usage:
 	ReCalc [-vc] [EXPRESSION ...]
 	ReCalc [EXPRESSION ...]
 	ReCalc [-vc]
-	ReCalc [--verbose --commandline]
+	ReCalc [--verbose --commandline --version]
 
 Arguments:
 	EXPRESSION    optional expression to start with
@@ -23,6 +23,7 @@ Arguments:
 Options:
 	-v, --verbose      verbose logging
 	-c, --commandline  use the command line interface
+	--version          give the version of ReCalc
 
 It can do:
 
@@ -223,6 +224,8 @@ from atexit import register
 from warnings import warn, simplefilter
 from pickle import load, dump
 from decimal import Context
+
+version = "0.1.12"
 
 logger = logging.getLogger("ReCalc_logger")
 logger.setLevel(logging.DEBUG)
@@ -531,6 +534,14 @@ regular_expr = dict(
 )
 
 
+def double_list(l):
+	return([i for s in ((i, i) for i in l) for i in s])
+
+
+def flatten(l):
+	return((i for s in l for i in s))
+
+
 class CalculatorError(Exception):
 	pass
 
@@ -607,6 +618,86 @@ class NonRepeatingList(object):
 		'''
 
 		self.items.clear()
+
+
+class Unit(object):
+	'''
+	A class that represents different units
+	'''
+
+	base_units = ("meters", "m", "kilograms", "kg", "seconds", "s")
+	
+	distance_units = (
+		"kilometers", "km",
+		"centimeters", "cm",
+		"milimeters", "mm",
+		"inches", "in",
+		"feet", "ft",
+		"yards", "yd",
+		"miles", "mi",)
+	mass_units = (
+		"grams", "g",
+		"pounds", "lbs",
+	)
+	time_units = (
+		"minutes", "min",
+		"hours", "h",
+	)
+
+	multipliers_distance = (
+		0.001, 100, 1000, 39.370078740157, 3.2808398950131,
+		1.0936132983377, 0.00062137119223733,
+	)
+	multipliers_mass = (
+		0.001, 2.2046226218,
+	)
+	multipliers_time = (
+		1/60, 1/3600, 
+	)
+
+	nonbase_units = {
+		"distance": distance_units,
+		"time": time_units,
+		"mass": mass_units
+	}
+	multipliers = {
+		"distance": multipliers_distance,
+		"time": multipliers_time,
+		"mass": multipliers_mass,
+	}
+
+	for i in multipliers:
+		multipliers[i] = double_list(multipliers[i])
+
+	from_base_funcs = {unit: lambda a: a for unit in base_units}
+	for unit, mult in zip(flatten(nonbase_units.values()), flatten(multipliers.values())):
+		from_base_funcs.update({unit: lambda a, c = mult: a * c})
+	
+	to_base_funcs = {}
+	for unit, mult in zip(flatten(nonbase_units.values()), flatten(multipliers.values())):
+		to_base_funcs.update({unit: lambda a, c = mult: a / c})
+
+	def __init__(self, amount, type):
+		self.type = type
+		self.amount = amount
+
+	def __str__(self):
+		return("{a} {u}".format(a = self.amount, u = self.type))
+
+	def convert_to(self, new):
+		'''
+		Change what unit the quantity is expressed as.
+		'''
+
+		if self.type in Unit.base_units:
+			self.amount = Unit.from_base_funcs[new](self.amount)
+		elif new in Unit.base_units:
+			self.amount = Unit.to_base_funcs[self.type](self.amount)
+		else:
+			self.amount = Unit.from_base_funcs[new](Unit.to_base_funcs[self.type](self.amount))
+
+		self.type = new
+		return(self)
 
 
 class Graph(object):
@@ -2858,7 +2949,10 @@ def main():
 
 	if "docopt" in sys.modules:
 		args = docopt(__doc__)
-
+		
+		if args["--version"]:
+			print(version)
+			sys.exit()
 		if args["-c"] or args["--commandline"]:
 			use_gui = False
 		if args["-v"] or args["--verbose"]:
--- calc_experimentation.py
+++ calc_experimentation.py
@@ -1,13 +1,89 @@
 # calc_experimentation.py
-import re
 
-units = ("meters", "kilometers")
+def double_list(l):
+	return([i for s in ((i, i) for i in l) for i in s])
+def flatten(l):
+	return((i for s in l for i in s))
 
-# intentianly not using compile_ignore_case so that you can
-# differentiate between capital and lowercase units
-conv_comp = re.compile(
-	"[Cc][Oo][Nn][Vv][Ee][Rr][Tt] (.+?)(?="
-	+ "|".join(units) + ")(" + "|".join(units) + ") ?[Tt][Oo] ("
-	+ "|".join(units) + ")")
-m = re.search(conv_comp, "convert 4 meters to kilometers")
-print(m.groups())
+class Unit(object):
+	'''
+	A class that represents different units
+	'''
+
+	base_units = ("meters", "m", "kilograms", "kg", "seconds", "s")
+	
+	distance_units = (
+		"kilometers", "km",
+		"centimeters", "cm",
+		"milimeters", "mm",
+		"inches", "in",
+		"feet", "ft",
+		"yards", "yd",
+		"miles", "mi",)
+	mass_units = (
+		"grams", "g",
+		"pounds", "lbs",
+	)
+	time_units = (
+		"minutes", "min",
+		"hours", "h",
+	)
+
+	multipliers_distance = (
+		0.001, 100, 1000, 39.370078740157, 3.2808398950131,
+		1.0936132983377, 0.00062137119223733,
+	)
+	multipliers_mass = (
+		0.001, 2.2046226218,
+	)
+	multipliers_time = (
+		1/60, 1/3600, 
+	)
+
+	nonbase_units = {
+		"distance": distance_units,
+		"time": time_units,
+		"mass": mass_units
+	}
+	multipliers = {
+		"distance": multipliers_distance,
+		"time": multipliers_time,
+		"mass": multipliers_mass,
+	}
+
+	for i in multipliers:
+		multipliers[i] = double_list(multipliers[i])
+
+	from_base_funcs = {unit: lambda a: a for unit in base_units}
+	for unit, mult in zip(flatten(nonbase_units.values()), flatten(multipliers.values())):
+		from_base_funcs.update({unit: lambda a, c = mult: a * c})
+	
+	to_base_funcs = {}
+	for unit, mult in zip(flatten(nonbase_units.values()), flatten(multipliers.values())):
+		to_base_funcs.update({unit: lambda a, c = mult: a / c})
+
+	def __init__(self, amount, type):
+		self.type = type
+		self.amount = amount
+
+	def __str__(self):
+		return("{a} {u}".format(a = self.amount, u = self.type))
+
+	def convert_to(self, new):
+		'''
+		Change what unit the quantity is expressed as.
+		'''
+
+		if self.type in Unit.base_units:
+			self.amount = Unit.from_base_funcs[new](self.amount)
+		elif new in Unit.base_units:
+			self.amount = Unit.to_base_funcs[self.type](self.amount)
+		else:
+			self.amount = Unit.from_base_funcs[new](Unit.to_base_funcs[self.type](self.amount))
+
+		self.type = new
+		return(self)
+
+
+a = Unit(4, "feet")
+print(a.convert_to("inches"))
