a9ba6ad6e35dbc1b3154e90bca70aeac2c28723a
Max Friedman <mr.awesome10000@gmail.com>
2018-04-17 16:48:55 -0700

    ReCalc webpage. Trigfunc buttons

--- server_for_ReCalc.py
+++ server_for_ReCalc.py
@@ -1,4 +1,10 @@
 # server_for_ReCalc.py
 
 import flask
-app = flask.Flask(__name__)
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def handle_requests(data ="pineapple"):
+	datafromjs = request.form['mydata']
+	return(datafromjs)
